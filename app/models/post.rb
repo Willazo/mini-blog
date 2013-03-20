1a4b7f00930158db02a69d1183a326d27a080578
Willazo <willfrell@gmail.com>
2013-03-20 00:11:29 +0000

    Gestionando el modelo de datos con migraciones

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314151221) do
+ActiveRecord::Schema.define(:version => 20130319233422) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "category"
   end
 
 end
