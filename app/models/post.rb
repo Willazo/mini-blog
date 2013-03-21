b51989be8adb19f2e00ec17cf879b79b6741edcb
Willazo <willfrell@gmail.com>
2013-03-21 00:30:04 +0000

    Conexión con la base de datos, validadores y eventos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title, :category
+
+  validates :title, :category, :presence => true
+  validates :title, :uniqueness => true
+
+  before_validation :apply_title_format
+
+private
+
+  def apply_title_format
+  	self[:title] = self[:title].strip.capitalize
+  end
+
 end
